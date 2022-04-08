6aaf1bd2191fece11b9b42470be4f5390defc0d0
Luan <luan.wb@aluno.ifsc.edu.br>
2022-04-08 11:44:04 +0000

    main

--- code/index.js
+++ code/index.js
@@ -18,32 +18,14 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload() {
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude',
-        'assets/dude.png',
-        { frameWidth: 32, frameHeight: 48 }
-    );
+function preload()
+{
 }
 
-function create() {
-    this.add.image(400, 300, 'sky');
-    this.add.image(600, 100, 'star');
-
-    platforms = this.physics.add.staticGroup();
-
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
-
-
-
+function create()
+{
 }
 
-function update() {
+function update()
+{
 }
