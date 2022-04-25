b8407de9e84b891f8cf61d3fd9cd5b0f3a05aaef
Luan <luan.wb@aluno.ifsc.edu.br>
2022-04-25 11:49:10 +0000

    Professor olhar

---
+++ cliente/code/cena1.js
@@ -0,0 +1,13 @@
+
+// Criar a cena 0
+const cena1 = new Phaser.Scene("Cena 1");
+
+
+cena1.preload = function () {};
+
+cena1.create = function () {};
+
+cena1.update = function () {};
+
+// Exportar a cena
+export { cena1 };
--- servidor.js
+++ servidor.js
@@ -3,5 +3,5 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(express.static("./code"));
+app.use(express.static("./cliente"));
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
