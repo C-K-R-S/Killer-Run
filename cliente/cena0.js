cf98d1abf0b4e4d338b442335b47b858d57d547b
Moniq <monique.pj@aluno.ifsc.edu.br>
2022-04-25 11:01:55 +0000

    atualização

--- cliente/cena0.js
+++ cliente/cena0.js
@@ -1,31 +1,36 @@
-// Importar a próxima cena 
+// Importar a próxima cena
 import { cena1 } from "./cena1.js";
+
 // Criar a cena 0
 const cena0 = new Phaser.Scene("Cena 0");
-var abertura; 
+var abertura;
 var button;
 
 cena0.preload = function () {
-  // Imagem de fundo  
+  // Imagem de fundo
   this.load.image("start", "./assets/start.png");
   this.load.audio("abertura", "./sounds/wait.mp3");
 };
 
 cena0.create = function () {
-    //Som de abertura
-    abertura = this.sound.add("abertura");
-    abertura.play();
-    abertura.setLoop(true);
-    // Botão com a imagem de fundo
-   button = this.add.image(400, 400, "start", 0).setInteractive();
+  //Som de abertura
+  abertura = this.sound.add("abertura");
+  abertura.play();
+  abertura.setLoop(true);
+
+  // Botão com a imagem de fundo
+  button = this.add.image(400, 400, "start", 0).setInteractive();
+
   // Ao clicar no botão, inicia a cena 1
-    button.on( "pointerdown", function () {
-        this.scene.start(cena1);
-        //para a musica
-        abertura.stop();
+  button.on(
+    "pointerdown",
+    function () {
+      this.scene.start(cena1);
+      //para a musica
+      abertura.stop();
     },
-        this
-    );
+    this
+  );
 };
 
 cena0.update = function () {};
