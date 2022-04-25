66484799c198ef5142ef02f4955eb9c9b66233e5
Ederson Torresini <boidacarapreta@gmail.com>
2022-04-25 13:40:00 -0300

    Formatar código

--- cliente/code/index.js
+++ cliente/code/index.js
@@ -1,27 +1,25 @@
 import { cena0 } from "./cena0.js";
 import { cena1 } from "./cena1.js";
 
-
-
 const config = {
-    type: Phaser.AUTO,
+  type: Phaser.AUTO,
+  width: 800,
+  height: 800,
+  parent: "game-container",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 },
+    },
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    parent: "game-container",
+    autoCenter: Phaser.Scale.CENTER_BOTH,
     width: 800,
     height: 800,
-    parent: "game-container",
-    physics: {
-      default: "arcade",
-      arcade: {
-        gravity: { y: 0 },
-      },
-    },
-    scale: {
-      mode: Phaser.Scale.FIT,
-      parent: "game-container",
-      autoCenter: Phaser.Scale.CENTER_BOTH,
-      width: 800,
-      height: 800,
-    },
-    scene: [cena0, cena1],
-  };
+  },
+  scene: [cena0, cena1],
+};
 
 const game = new Phaser.Game(config);
